e1413d0c951451cd80bb99b983f4ee0eaf4574c8
whitehyun <cx_nf@naver.com>
2020-10-29 01:59:33 +0900

    HW2: Change server_name value

--- UDP_ping_client.py
+++ UDP_ping_client.py
@@ -3,7 +3,7 @@ import time
 
 IP_ADD = 0  # 클라이언트 주소의 주소와 포트 중 주소 인덱스를 가리킴
 if __name__ == "__main__":
-    server_name = '127.0.0.3'  # Desktop 내부주소 언제든 변경 가능
+    server_name = '192.168.0.3'  # Desktop 내부주소 언제든 변경 가능
     server_port = 12000
     data_length = 1000  # 데이터 길이 많은 데이터를 보내고 싶을 경우 이를 수정하면 됨
     server_address = 0  # 서버 주소 미리 선언(unbound를 피하기 위함)
